e09f2eb0544dc433fb4e90162a1a81780b501427
iamareebjamal <jamal.areeb@gmail.com>
2017-11-22 21:59:54 +0530

    feat: Add notification handling

--- app/src/main/java/io/amu/oss/ui/MainActivity.kt
+++ app/src/main/java/io/amu/oss/ui/MainActivity.kt
@@ -1,14 +1,16 @@
-package io.amu.oss
+package io.amu.oss.ui
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import io.amu.oss.R
 
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.AnkoLogger
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
