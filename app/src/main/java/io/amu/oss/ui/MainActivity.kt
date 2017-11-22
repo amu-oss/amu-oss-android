50046debed3588ea5cd01518c30aa10d46db8bb2
iamareebjamal <jamal.areeb@gmail.com>
2017-11-22 23:56:44 +0530

    feat: Allow user to subscribe and unsubscribe from topic

--- app/src/main/java/io/amu/oss/ui/MainActivity.kt
+++ app/src/main/java/io/amu/oss/ui/MainActivity.kt
@@ -1,31 +1,63 @@
 package io.amu.oss.ui
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.support.graphics.drawable.VectorDrawableCompat
+import android.support.v4.graphics.drawable.DrawableCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import io.amu.oss.OpenApplication
 import io.amu.oss.R
+import io.amu.oss.ui.presenter.MainPresenter
+import io.amu.oss.ui.view.MainView
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.toast
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity(), AnkoLogger {
+class MainActivity : AppCompatActivity(), MainView, AnkoLogger {
+
+    @Inject lateinit var mainPresenter: MainPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        (application as OpenApplication).component.inject(this)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        mainPresenter.setView(this)
+
+        fab.setOnClickListener {
+            mainPresenter.controller.clickSubscribe()
         }
     }
 
+    override fun showToast(message: String) {
+        toast(message)
+    }
+
+    override fun subscribed(subscribed: Boolean) {
+        if (subscribed) subscribed() else unsubscribed()
+    }
+
+    private fun subscribed() {
+        fab.setImageDrawable(VectorDrawableCompat.create(resources, R.drawable.ic_close, theme))
+        subscribeStatus.text = "Unsubscribe"
+        toast("Subscribed")
+    }
+
+    private fun unsubscribed() {
+        fab.setImageDrawable(VectorDrawableCompat.create(resources, R.drawable.ic_check, theme))
+        subscribeStatus.text = "Subscribe"
+        toast("Unsubscribed")
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
+        // Disabled menu for Future
+        // menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
